0eb74ab0c202aa0b2faa876360492e23b6ddbb66
UJJAWAL CHAUHAN <ujjaval765@gmail.com>
2025-09-17 12:00:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Doubly Linked List Traversal/doubly-linked-list-traversal.java
@@ -0,0 +1,27 @@
+// User function Template for Java
+
+/*
+class Node
+{
+    int data;
+    Node next, prev;
+    Node(int data)
+    {
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+    }
+}
+*/
+class Solution {
+    public static ArrayList<Integer> displayList(Node head) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Node temp=head;
+        while(temp!=null){
+            list.add(temp.data);
+            temp=temp.next;
+        }
+        return list;
+        
+    }
+}
