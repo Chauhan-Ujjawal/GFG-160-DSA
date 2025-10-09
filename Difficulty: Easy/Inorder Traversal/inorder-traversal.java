4d1b71591a55878d9638f2489730eb17cb8b8c3f
UJJAWAL CHAUHAN <ujjaval765@gmail.com>
2025-10-09 15:18:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Inorder Traversal/inorder-traversal.java
+++ Difficulty: Easy/Inorder Traversal/inorder-traversal.java
@@ -1,27 +1,45 @@
-/* A Binary Tree node
-
+/*
 class Node {
     int data;
     Node left, right;
-   Node(int item)    {
+    Node(int item){
         data = item;
         left = right = null;
     }
 }
 */
 class Solution {
-    // Function to return a list containing the inorder traversal of the tree.
-    ArrayList<Integer> inOrder(Node root) {
-        ArrayList<Integer> list = new ArrayList<>();
-        helper(root, list);
-        return list;
+    public ArrayList<Integer> inOrder(Node root) {
+        // using morris traversal
+        ArrayList<Integer> inorder = new ArrayList<>();
+        Node current = root;
+        while(current!=null){
+            if(current.left==null){
+                inorder.add(current.data);
+                current=current.right;
+            }
+            else{
+                //going at rightmost node of left subtree 
+                Node runner=current.left;
+                while(runner.right!=null && runner.right!=current){
+                    runner=runner.right;
+                }
+                //reached rightmost node
+                if(runner.right==null){
+                    //if righgtmost is null thread it and move current to left
+                    runner.right=current;
+                    current=current.left;
+                }
+                else{
+                    //if rightmost is not null then it has been visited and remove 
+                    //thread and move right
+                    runner.right=null;
+                    inorder.add(current.data);
+                    current=current.right;
+                }
+            }
+        }
+        return inorder;
+        
     }
-
-    void helper(Node root, ArrayList<Integer> list) {
-        if (root == null) return;
-        helper(root.left, list);
-        list.add(root.data);
-        helper(root.right, list);
-    }
-
 }
