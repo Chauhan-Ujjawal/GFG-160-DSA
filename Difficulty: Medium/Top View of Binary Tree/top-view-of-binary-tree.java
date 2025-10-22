141fa5df783c9077a1ee3c4d69f6d1a5baa6891a
UJJAWAL CHAUHAN <ujjaval765@gmail.com>
2025-10-22 11:00:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.java
@@ -1,40 +1,50 @@
 /*
-class Node{
+class Node {
     int data;
-    Node left;
-    Node right;
-    Node(int data){
-        this.data = data;
-        left=null;
-        right=null;
+    Node left, right;
+
+    Node(int val) {
+        this.data = val;
+        this.left = null;
+        this.right = null;
     }
 }
 */
-
 class Solution {
-    // Function to return a list of nodes visible from the top view
-    // from left to right in Binary Tree.
     static class Pair{
-        int index;
+        int hd;
         Node node;
-        public Pair(int index,Node node){
-            this.index=index;
+        public Pair(int hd,Node node){
+            this.hd=hd;
             this.node=node;
         }
     }
-    static ArrayList<Integer> topView(Node root) {
-        Queue<Pair> q = new ArrayDeque <>();
-        Map<Integer,Integer> map = new TreeMap<>();
-        q.add(new Pair(0,root));
+
+    public ArrayList<Integer> topView(Node root) {
+        //make a map of integer,arrayList
+        //make a pair class to save root node and horizontal distance
+        //push root node 
+        //run a while loop
+        //if current hd is in map then append element in  corresponding array list
+        //else create a new hd and new array list \
+        //add left children 
+        //add right children
+        Map<Integer,Integer> map= new TreeMap<>();
+        Queue<Pair> q = new ArrayDeque<>();
+        q.offer(new Pair(0,root));
         while(!q.isEmpty()){
             Pair current = q.poll();
-            if(!map.containsKey(current.index)){
-                map.put(current.index,current.node.data);
+            //storing the element that are only first time appearing on a particular index
+            if(!map.containsKey(current.hd)){
+                map.put(current.hd,current.node.data);
             }
-            if(current.node.left!=null) q.add(new Pair(current.index-1,current.node.left));
-            if(current.node.right!=null) q.add(new Pair(current.index+1,current.node.right));
+            if(current.node.left!=null){
+                q.offer(new Pair(current.hd-1,current.node.left));
+            }
+            if(current.node.right!=null){
+                q.offer(new Pair(current.hd+1,current.node.right));
+            }
         }
-        ArrayList<Integer> ans = new ArrayList<>(map.values());
-        return ans;
+        return new ArrayList<>(map.values());
     }
 }
